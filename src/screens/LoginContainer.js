c4539ebc7a8cd8232d3fdeafce61ad3900495f5e
Sahad <sahadwg@gmail.com>
2024-04-15 21:35:20 +0530

    basic google login via firebase -SHA added

--- src/screens/LoginContainer.js
+++ src/screens/LoginContainer.js
@@ -1,11 +1,32 @@
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
 
 const LoginContainer = () => {
+  GoogleSignin.configure({
+    webClientId:
+      '855982703073-0v1f1f9phnm2n94cmu7kbcrkcg50jjn1.apps.googleusercontent.com',
+  });
+
+  const onGoogleButtonPress = async () => {
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    const {idToken} = await GoogleSignin.signIn();
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    return auth().signInWithCredential(googleCredential);
+  };
+
   return (
-    <View>
-      <Text>LoginContainer</Text>
-    </View>
+    <>
+      <Button
+        title="Google Sign-In"
+        onPress={() =>
+          onGoogleButtonPress().then(() =>
+            console.log('Signed in with Google!'),
+          )
+        }
+      />
+    </>
   );
 };
 
